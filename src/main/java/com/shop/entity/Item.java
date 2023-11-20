d6b15f5ca201fd8907b7f9a68f031ee83b26008f
nscoo <nscoo@naver.com>
2023-11-20 23:05:15 +0900

    init

---
+++ src/main/java/com/shop/entity/Item.java
@@ -0,0 +1,23 @@
+package com.shop.entity;
+
+import com.shop.constant.ItemSellStatus;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@ToString
+public class Item {
+    private Long id;
+    private String itemNm;
+    private int price;
+    private int stockNumber;
+    private String itemDetail;
+    private ItemSellStatus itemSellStatus;
+    private LocalDateTime regTime;
+    private LocalDateTime updateTime;
+
+}
